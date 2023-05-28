664faef353c44218633d5ede5b2cd95b0895ae64
WasinUddy <wasinsilakong@gmail.com>
2023-05-28 20:52:02 +0700

    refactor

--- garray.py
+++ garray.py
@@ -1,112 +1,92 @@
 import json
 from tiles import Tiles
 
+
 class GArray:
     """
-    Game Array class to handle 2D Game events and manipulations before being use for rendering
+    Game Array class to handle 2D game events and manipulations before being used for rendering.
     """
 
-    def __init__(self, game_map: str):
+    def __init__(self, game_map_file: str):
         """
-        Class constructor.
+        Initialize the GArray instance.
 
         Args:
-            game_map (str): The game map to be used for rendering load from the file
+            game_map_file (str): The file path of the game map to be used for rendering.
         """
-        self.load_map(game_map)    # Load the game map from the file Generate self.data, self.width, self.height
+        self.load_map(game_map_file)
 
-
-    def load_map(self, game_map: str):
+    def load_map(self, game_map_file: str):
         """
-        Load the game map from the file
-        
-        game_map format
-        {
-            "map": {
-                "x;y": {
-                    "0": "tile_type",
-                    "1": "tile_type",
-                }
-            }
-            "tile_path": {
-                "tile_type": "path/to/tile"
-            }
-        }
-        """
+        Load the game map from the file.
 
-        with open(game_map, "r") as f:
+        Args:
+            game_map_file (str): The file path of the game map.
+        """
+        with open(game_map_file, "r") as f:
             data = json.load(f)
-        
-        gmap = data["map"]
-        #tils_path = data["tile_path"]
 
-        # Get the size of the map
-        # Max X
-        max_x, max_y = 0, 0
-        for key in gmap.keys():
-            x , y = tuple(map(int, key.split(";")))
-            if x > max_x:
-                max_x = x
-            if y > max_y:
-                max_y = y
-        
-        self.height = max_y + 1
+        gmap = data.get("map", {})
+        # tile_paths = data.get("tile_path", {})
+
+        max_x = max_y = 0
+        for key in gmap:
+            x, y = map(int, key.split(";"))
+            max_x = max(max_x, x)
+            max_y = max(max_y, y)
+
         self.width = max_x + 1
+        self.height = max_y + 1
 
-        # Create the array
-        self.data = [[None for _ in range(self.width)] for _ in range(self.height)]
-        for y in range(self.height):
-            for x in range(self.width):
-                if f"{x};{y}" in gmap.keys():
-                    self.data[y][x] = Tiles(gmap[f"{x};{y}"]["0"][0])       # Select only the first layer of the tile will FIX LATER
-                else:
-                    self.data[y][x] = Tiles("VOID")
+        self.data = [[Tiles("VOID") for _ in range(self.width)] for _ in range(self.height)]
+        for key, tile_data in gmap.items():
+            x, y = map(int, key.split(";"))
+            if "0" in tile_data:
+                self.data[y][x] = Tiles(tile_data["0"][0])
 
-    
     def ripple(self, pos: tuple):
         """
-        Activate surrounding tiles of the given position similar to a ripple effect
+        Activate surrounding tiles of the given position in a ripple effect.
 
         Args:
-            pos (tuple): The position of the tile to be activated
+            pos (tuple): The position of the tile to be activated.
         """
-
-        # Activate side function of the tile of all 4 surrounding tiles
         x, y = pos
-        if x > 0:
-            self.data[y][x-1].side()
-        if x < self.width - 1:
-            self.data[y][x+1].side()
-        if y > 0:
-            self.data[y-1][x].side()
-        if y < self.height - 1:
-            self.data[y+1][x].side()
 
-        # Activate custom function of the tile of all 4 surrounding tiles
+        # Activate side function of the surrounding tiles
         if x > 0:
-            self.data[y][x-1].up()
+            self.data[y][x - 1].side()
         if x < self.width - 1:
-            self.data[y][x+1].down()
-        if y > 0: 
-            self.data[y-1][x].left()
+            self.data[y][x + 1].side()
+        if y > 0:
+            self.data[y - 1][x].side()
         if y < self.height - 1:
-            self.data[y+1][x].right()
-        
-        # Activate custom function of the tile of the given position
+            self.data[y + 1][x].side()
+
+        # Activate custom function of the surrounding tiles
+        if x > 0:
+            self.data[y][x - 1].up()
+        if x < self.width - 1:
+            self.data[y][x + 1].down()
+        if y > 0:
+            self.data[y - 1][x].left()
+        if y < self.height - 1:
+            self.data[y + 1][x].right()
+
+        # Activate custom function of the tile at the given position
         self.data[y][x].here()
 
+    def __str__(self):
+        """
+        Return the string representation of the game array in a formatted table with summarized data.
 
-        
+        Returns:
+            str: The string representation of the game array.
+        """
+        max_length = max(len(str(tile)) for row in self.data for tile in row)
 
-        
+        table = ""
+        for row in self.data:
+            table += " ".join(f"{str(tile):<{max_length}}" for tile in row) + "\n"
 
-
-                
-
-
-
-if __name__ == "__main__":
-    g = GArray("map.json")
-    print(g.data)
-    print(g.width, g.height)
-    print(g.data[0][0   ])
+        return table + f"\nWidth: {self.width}\nHeight: {self.height}"
--- tiles.py
+++ tiles.py
@@ -1,7 +1,7 @@
 class Tiles:
     """
-    Base class for every tile in the game and can be override to create custom tiles to coperate with the game.
-    The tile will be able to interact with character eg character on the top of the tile will affect the character's speed.
+    Base class for every tile in the game and can be overridden to create custom tiles to cooperate with the game.
+    The tile will be able to interact with characters, e.g., a character on top of the tile may affect the character's speed.
     """
 
     def __init__(self, tile_type: str):
@@ -9,17 +9,15 @@ class Tiles:
         Class constructor.
 
         Args:
-            tile_type (str): The type of the tile to be selected for rendering
+            tile_type (str): The type of the tile to be selected for rendering.
         """
         self.tile_type = tile_type
 
-    
     def up(self):
         """
-        The effect of the tile when the character is on the top of the tile.
+        The effect of the tile when the character is on top of the tile.
         """
         pass
-
 
     def down(self):
         """
@@ -27,13 +25,11 @@ class Tiles:
         """
         pass
 
-
     def left(self):
         """
         The effect of the tile when the character is on the left of the tile.
         """
         pass
-
 
     def right(self):
         """
@@ -46,7 +42,7 @@ class Tiles:
         The effect of the tile when the character is on the tile.
         """
         pass
-    
+
     def side(self):
         """
         The effect of the tile when the character is on the side of the tile.
@@ -54,10 +50,11 @@ class Tiles:
         pass
 
     def __str__(self):
+        """
+        Return the string representation of the tile.
+
+        Returns:
+            str: The string representation of the tile.
+        """
         return self.tile_type
-
-
-
-
-
     
